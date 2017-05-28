e0ec869460fa34b980746856ab4e030abe5a9e11
Gery Egan <gery.egan@gmail.com>
2017-05-27 22:54:47 -0700

    deck update animation spring implemented

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder, Text, Dimensions } from 'react-native';
+import { View, Animated, PanResponder, Text, Dimensions, LayoutAnimation, UIManager } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
@@ -31,6 +31,11 @@ class Deck extends Component {
 
         this.state =  { panResponder, position, index: 0 }
         
+    }
+
+    componentWillUpdate() {
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.spring();
     }
 
     forceSwipe(direction) {
